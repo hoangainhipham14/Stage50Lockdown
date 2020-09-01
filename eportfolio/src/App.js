845a76014edb03379abaa9bb987edebec3055c42
OutstandingMove <phamb1@student.unimelb.edu.au>
2020-09-01 16:05:44 +1000

    Exercise Tracker template, npm run dev to start

---
+++ eportfolio/src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+// function Moff(props) {
+//   return (
+//     <a
+//       href="https://www.youtube.com/watch?v=suu4hdD3qd4"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       <button>Get Moffed</button>
+//     </a>
+//   );
+// }
+
+function App() {
+  //   render() {
+  //     let message;
+  //     if (this.state.clicks >= 69) {
+  //       message = "Nice.";
+  //     } else if (this.state.clicks >= 420) {
+  //       message = "Go outside or something holy shit.";
+  //     } else {
+  //       message = "";
+  //     }
+
+  //   return (
+  //     <div className="clicker">
+  //       <h2>Counter</h2>
+  //       <button
+  //         onClick={() => }
+  //       >
+  //         Click me!
+  //       </button>
+  //       {/* <p>Clicks: {this.state.clicks}</p> */}
+  //       <p>{"message"}</p>
+  //     </div>
+  //   );
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- eportfolio/src/index.js
+++ eportfolio/src/index.js
@@ -1,47 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import App from "./App";
 
-function Moff(props) {
-    return (
-        <a href="https://www.youtube.com/watch?v=suu4hdD3qd4" target="_blank" rel="noopener noreferrer"><button>Get Moffed</button></a>
-    )
-}
-
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clicks: 0,
-        };
-    }
-
-    render() {
-        let message;
-        if (this.state.clicks >= 69) {
-            message = "Nice."
-        } else if (this.state.clicks >= 420) {
-            message = "Go outside or something holy shit."
-        } else {
-            message = "";
-        }
-
-        return(
-            <div className="clicker">
-                <h2>Counter</h2>
-                <button onClick={() => this.setState({clicks: this.state.clicks + 1})}>Click me!</button>
-                <p>Clicks: {this.state.clicks}</p>
-                <p>{message}</p>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <div className="demo">
-        <h1>Hello world!</h1>
-        <Counter />
-        <Moff />
-    </div>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
