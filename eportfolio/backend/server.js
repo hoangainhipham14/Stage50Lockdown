9e815f9cd75894e9381ed5b3d99021c927f3fd66
OutstandingMove <phamb1@student.unimelb.edu.au>
2020-09-02 16:25:48 +1000

    Change as per Nhi's request

--- eportfolio/backend/server.js
+++ eportfolio/backend/server.js
@@ -11,7 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
