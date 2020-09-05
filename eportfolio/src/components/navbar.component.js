7ca9d09578f1649d2bdc1f843a97d335b51ce3ba
rwarneke <rowanwarneke@gmail.com>
2020-09-05 21:00:10 +1000

    Added constructor to navbar

--- eportfolio/src/components/navbar.component.js
+++ eportfolio/src/components/navbar.component.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Loaded navbar");
+  }
 
   render() {
     return (
