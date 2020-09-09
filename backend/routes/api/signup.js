4188aab86837f818b9ca6771a08be33a492d84c3
Mitchell Rule <mrule1@student.unimelb.edu.au>
2020-09-09 16:54:55 +1000

    Made email validation magic link

--- backend/routes/api/signup.js
+++ backend/routes/api/signup.js
@@ -1,47 +1,47 @@
 // For the email verification
 ///
-var crypto = require('crypto');
-var nodemailer = require('nodemailer');
+var crypto = require("crypto");
+var nodemailer = require("nodemailer");
 ///
 
-const signup = require('express').Router();
-let User = require('../../models/user.model');
-let UserSession = require('../../models/userSession');
-let UserToken = require('../../models/userToken');
+const signup = require("express").Router();
+let User = require("../../models/user.model");
+let UserSession = require("../../models/userSession");
+let UserToken = require("../../models/userToken");
 
-signup.route('/').post((req, res) => {
-    const firstname = req.body.firstName;
-    const lastname = req.body.lastName;
-    const password = req.body.password;
-    email = req.body.email;
+signup.route("/").post((req, res) => {
+  const firstname = req.body.firstName;
+  const lastname = req.body.lastName;
+  const password = req.body.password;
+  email = req.body.email;
 
-    console.log(firstname, lastname, email, password);
+  console.log(firstname, lastname, email, password);
 
   if (!firstname) {
     return res.send({
-          success: false,
-          message: "First name is required"
+      success: false,
+      message: "First name is required",
     });
   }
 
   if (!lastname) {
     return res.send({
-          success: false,
-          message: "Last name is required"
+      success: false,
+      message: "Last name is required",
     });
   }
 
   if (!email) {
     return res.send({
-          success: false,
-          message: "Email is required"
+      success: false,
+      message: "Email is required",
     });
   }
 
   if (!password) {
     return res.send({
-          success: false,
-          message: "Password is required"
+      success: false,
+      message: "Password is required",
     });
   }
 
@@ -51,7 +51,7 @@ signup.route('/').post((req, res) => {
     email: email,
   }).catch((err, preUsers) => {
     if (preUsers.length > 0) {
-        return res.end("Error: Account already exists")
+      return res.end("Error: Account already exists");
     } else {
       res.end("Error");
     }
@@ -63,55 +63,64 @@ signup.route('/').post((req, res) => {
   newUser.email = email;
   newUser.password = newUser.generateHash(password);
 
-  // This is where the fun starts 
+  // This is where the fun starts
   //console.log('Account being created...');
   //newUser.save().catch(err => res.status(400).json('Error: ' + err));
 
-  var token = new UserToken({ _userId: newUser._id, token: crypto.randomBytes(16).toString('hex') });
+  var token = new UserToken({
+    _userId: newUser._id,
+    token: crypto.randomBytes(16).toString("hex"),
+  });
   //console.log('Token Generated....');
 
   // Save the verification token
-  token.save().catch(err => res.status(500).send({ msgFromTokenSave: err.message })); 
+  token
+    .save()
+    .catch((err) => res.status(500).send({ msgFromTokenSave: err.message }));
   //console.log('Token Saved....');
 
   // Create the email (With information from .env file)
-  var transporter = nodemailer.createTransport({ 
-    service: 'gmail', 
-    auth: { 
-    user: process.env.GMAIL_USERNAME, 
-    pass: process.env.GMAIL_PASSWORD 
-    } 
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
+    },
   });
   //console.log('Email Created....');
 
   // Specify the email contents
   var mailOptions = {
-    from: 'noreply',
-    to: newUser.email, 
-    subject: 'Account Verification Token', 
-    text: 'Verify your account by clicking the link (Will be required in the near future): \nhttp:\/\/' 
-    + req.headers.host + '\/users/validation\/\n and entering the token: ' + token.token, 
+    from: "noreply",
+    to: newUser.email,
+    subject: "Account Verification Token",
+    text:
+      "Verify your account by clicking the link (Will be required in the near future): \nhttp://" +
+      req.headers.host +
+      "/users/validation/" +
+      token.token,
   };
 
   // Send the email
-  transporter.sendMail(mailOptions).catch(err => res.status(500).send({ errMsgFromSendMail: err.message }));
+  transporter
+    .sendMail(mailOptions)
+    .catch((err) => res.status(500).send({ errMsgFromSendMail: err.message }));
 
   newUser.save((err, user) => {
     if (err) {
       res.send({
         success: false,
-        message: "Error: Server error" + err.message
+        message: "Error: Server error" + err.message,
       });
     } else {
       res.send({
         success: true,
-        message: 'A verification email has been sent to ' + newUser.email + '.'
+        message: "A verification email has been sent to " + newUser.email + ".",
       });
-      console.log('Email Sent....');
+      console.log("Email Sent....");
     }
   });
   // res.status(200).send('A verification email has been sent to ' + newUser.email + '.');
-
 });
 
 module.exports = signup;
