468193440f5c2d59eff84f9a3203776437124b0b
hoangainhipham14 <hoangainhip@student.unimelb.edu.au>
2020-09-11 19:58:31 +1000

    resolve conflict

--- backend/routes/api/signup.js
+++ backend/routes/api/signup.js
@@ -57,83 +57,6 @@ signup.route('/').post((req, res) => {
 
   email = email.toLowerCase();
 
-<<<<<<< HEAD
-  User.find({
-    email: email,
-  }).catch((err, preUsers) => {
-    if (preUsers.length > 0) {
-      return res.end("Error: Account already exists");
-    } else {
-      res.end("Error");
-    }
-  });
-
-  const newUser = new User();
-  newUser.firstname = firstname;
-  newUser.lastname = lastname;
-  newUser.username = username;
-  newUser.email = email;
-  newUser.password = newUser.generateHash(password);
-
-  // This is where the fun starts
-  //console.log('Account being created...');
-  //newUser.save().catch(err => res.status(400).json('Error: ' + err));
-
-  var token = new UserToken({
-    _userId: newUser._id,
-    token: crypto.randomBytes(16).toString("hex"),
-  });
-  //console.log('Token Generated....');
-
-  // Save the verification token
-  token
-    .save()
-    .catch((err) => res.status(500).send({ msgFromTokenSave: err.message }));
-  //console.log('Token Saved....');
-
-  // Create the email (With information from .env file)
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
-  //console.log('Email Created....');
-
-  // Specify the email contents
-  var mailOptions = {
-    from: "noreply",
-    to: newUser.email,
-    subject: "Account Verification Token",
-    text:
-      "Verify your account by clicking the link (Will be required in the near future): \nhttp://" +
-      req.headers.host +
-      "/users/validation/\n and entering the token: " +
-      token.token,
-  };
-
-  // Send the email
-  transporter
-    .sendMail(mailOptions)
-    .catch((err) => res.status(500).send({ errMsgFromSendMail: err.message }));
-
-  newUser.save((err, user) => {
-    if (err) {
-      res.send({
-        success: false,
-        message: "Error: Server error" + err.message,
-      });
-    } else {
-      res.send({
-        success: true,
-        message: "A verification email has been sent to " + newUser.email + ".",
-      });
-      console.log("Email Sent....");
-    }
-  });
-  // res.status(200).send('A verification email has been sent to ' + newUser.email + '.');
-=======
   // proceed only if email is not already in database
   User.findOne({email: email}).then(user => {
     if (user) {
@@ -201,7 +124,6 @@ signup.route('/').post((req, res) => {
     }
   });
 
->>>>>>> master
 });
 
 module.exports = signup;
