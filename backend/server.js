a1706db46568dee2832071fb16b66329ff2dc53a
Mitchell Rule <mrule1@student.unimelb.edu.au>
2020-09-12 15:30:07 +1000

    changed static path

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ const uploadimg = require("./routes/api/uploadimg");
 app.use("/api/upload", uploadimg);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
