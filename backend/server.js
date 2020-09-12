d0b96056bcfbc516dbf5b5caa1a82d6836e61392
Mitchell Rule <mrule1@student.unimelb.edu.au>
2020-09-12 13:19:45 +1000

    Moved port const

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -60,7 +60,13 @@ app.use("/upload", uploadimg);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+  });
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
