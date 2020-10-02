7427737fb61a13903618620b41fb400549abc40b
OutstandingMove <phamb1@student.unimelb.edu.au>
2020-10-02 19:22:20 +1000

    Adding the production variable

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,14 @@ const projectRoutes = require("./routes/project");
 app.use("/api", authRoutes);
 app.use("/api", projectRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
+}
+
 // production
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
