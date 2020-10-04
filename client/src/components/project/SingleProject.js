8447ab3d7dda2186a13413de0ffc8c192c401a6d
hoangainhipham14 <hoangainhip@student.unimelb.edu.au>
2020-10-04 15:36:29 +1100

    Revert "Updated project item layout"
    This reverts commit e407034c34aa5e7e8f504e7e19eef7090dfa58e8.

--- client/src/components/project/SingleProject.js
+++ client/src/components/project/SingleProject.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import { singleProject } from "./APIProject";
-import {
-  Card,
-  Container,
-  Image,
-  Row,
-  Jumbotron,
-  Col,
-  ListGroup,
-} from "react-bootstrap";
+import { Card, Container, Image, Row, Jumbotron, Col } from "react-bootstrap";
 class SingleProject extends Component {
   state = {
     project: "",
@@ -34,37 +26,23 @@ class SingleProject extends Component {
       <Container>
         <Card bg="Light" border="secondary">
           <Card.Header>{project.title}</Card.Header>
-          <ListGroup variant="flush">
-            <ListGroup.Item> {project.body}</ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col sm={20}>
-                  <Image
-                    src={`/api/project/img/${this.state.projectId}`}
-                    alt={project.title}
-                    rounded
-                    thumbnail
-                  />
-                </Col>
-                <Col lg={true}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Quisque malesuada enim enim, non fermentum dui commodo ac.
-                  Aenean rhoncus turpis nisl, eu dictum sem molestie id. Morbi
-                  volutpat mattis ex vitae molestie. Proin id ante tempus,
-                  euismod nunc vel, suscipit diam. Praesent pretium tellus quis
-                  augue ultricies, sed congue erat gravida. Nulla tempor mattis
-                  eros, vitae pulvinar nunc congue vel. Vestibulum non mi
-                  tellus. Donec et mattis libero. Quisque eget urna nulla. Fusce
-                  sed rhoncus ante. Quisque sit amet purus magna. Quisque
-                  malesuada orci odio, vitae bibendum nulla porttitor ac. Nulla
-                  quis iaculis mauris, bibendum placerat quam. Etiam condimentum
-                  pellentesque suscipit. Aliquam erat volutpat. Mauris elit
-                  nisi, aliquet ac est a, congue vestibulum neque.
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          </ListGroup>
+          <Card.Body>
+            <Card.Text>{project.body}</Card.Text>
+          </Card.Body>
         </Card>
+        <Row>
+          <Col sm={20}>
+            <Image
+              src={`/api/project/img/${this.state.projectId}`}
+              alt={project.title}
+              fluid
+              rounded
+            />
+          </Col>
+          <Col lg={true}>
+            <Jumbotron>Project Details</Jumbotron>
+          </Col>
+        </Row>
 
         <Row className="justify-content-md-center">
           <Image
