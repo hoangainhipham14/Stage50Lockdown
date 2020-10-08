037d93de3cfc79db562d92c63a56e5fe7062b57d
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2020-10-08 19:01:18 +1100

    Made the project list look more like the mockup

--- client/src/components/dashboard/ProjectList.js
+++ client/src/components/dashboard/ProjectList.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class ProjectList extends Component {
@@ -16,6 +16,7 @@ class ProjectList extends Component {
 
     this.state = {
       projects: [],
+      tempId: "",
     };
   }
 
@@ -38,61 +39,60 @@ class ProjectList extends Component {
   };
 
   /*
-  onSubmit = (e) => {
-    e.preventDefault();
-    console.log("Button Clicked");
-    const projectId = this.props.match.params.projectId;
-    axios
-      .post(`/api/project/togglePrivacy/${projectId}`, { projectID: projectId })
-      .then((response) => {
-        if (response.error) {
-          console.log("failure");
-          console.log(response.error);
-        } else {
-          console.log("Button Clicked with id" + projectId);
-          //console.log("Project is now" + response.data);
-          //console.log(this.state.projects);
-        }
-      });
-  };
+  Note that I had to write this in HTML as I couldnt find an equally compatible
+  native boostrap counterpart
   */
-
+  /*
   render() {
     const projectCards = this.state.projects.map((project, idx) => (
       // project.linkRoute = "/projects/" + project._id;
       <Container>
-        <Row>
-          <Col>
-            <Link to={`/projects/${project._id}`}>
-              <ListGroup.Item key={idx}>{project.title}</ListGroup.Item>
-            </Link>
-          </Col>
-          <Col>
-            <Link to={`/projects/privacy/${project._id}`}>
-              <ListGroup.Item key={idx}>Change Privacy Setting</ListGroup.Item>
-            </Link>
-          </Col>
-        </Row>
+        <Card>
+          <Card.Body key={project._id}>
+            <Card.Title>{project.title}</Card.Title>
+            <Card.Body>{project.about}</Card.Body>
+          </Card.Body>
+        </Card>
+        <Link to={`/projects/privacy/${project._id}`}>
+          <Button>Change Privacy Settings</Button>
+        </Link>
+        <Link to={`/projects/${project._id}`}>
+          <Button>View Project</Button>
+        </Link>
       </Container>
+    ));
+*/
+
+  render() {
+    const projectCards = this.state.projects.map((project, idx) => (
+      <div>
+        <a
+          href={`/projects/${project._id}`}
+          class="list-group-item list-group-item-action"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{project.title}</h5>
+            <small>{project.created}</small>
+          </div>
+          <p class="mb-1">{project.about}</p>
+        </a>
+        <a href={`/projects/privacy/${project._id}`}>
+          <button type="button" class="btn btn-primary btn-sm">
+            Change Privacy Setting
+          </button>
+        </a>
+      </div>
     ));
 
     return (
-      <Container>
+      <div class="list-group">
         Project List (Will make nicer in the future)
         {projectCards}
-      </Container>
+      </div>
     );
   }
 }
 
-/**
- * 
- *         <Link to={`/projects/privacy/${project._id}`}>
-              <ListGroup.Item key={idx}>Change Privacy Setting</ListGroup.Item>
-            </Link>
- *    <Button onClick={this.onTogglePrivacyClick(project._id)}>Toggle Privacy Settings
-        </Button>
- */
 ProjectList.propTypes = {
   auth: PropTypes.object.isRequired,
 };
