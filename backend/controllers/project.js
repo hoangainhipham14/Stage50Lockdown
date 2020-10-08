96781b6e75f5fb7aea1ab88cdd5a83f168e0e8fe
Rowan Warneke <rowanwarneke@gmail.com>
2020-10-08 13:07:19 +1100

    incomplete backend for additional file upload

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -10,9 +10,22 @@ exports.createProject = (req, res, next) => {
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        image: "Image could not be uploaded",
+        image: "There was a problem creating your project",
       });
     }
+
+    console.log("Fields\n", fields);
+    console.log("Got", Object.keys(files).length, "files.");
+    console.log(Object.keys(files));
+
+    // number of additional files in the project
+    // all the files in the request are "additional files" except the main image
+    const numFiles = Object.keys(files).length - (files.image ? 1 : 0);
+    for (var i = 0; i < numFiles; i++) {
+      console.log(files[`file-${i}`]);
+    }
+
+    // NEED TO WORK OUT HOW TO SAVE THE FILES
 
     // create the new project
     let project = new Project(fields);
