d1565d04f32f93ff124a670d1a210a21dd21492b
Rowan Warneke <rowanwarneke@gmail.com>
2020-10-20 00:19:59 +1100

    added additional file display to project view, as well as rich text

--- client/src/components/project/SingleProject.js
+++ client/src/components/project/SingleProject.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { singleProject, connectLinkToProject } from "./APIProject";
 import { Card, Container, Image, Row, Col, ListGroup } from "react-bootstrap";
+import { HCenter } from "../layout";
 class SingleProject extends Component {
   state = {
     project: "",
@@ -30,6 +31,7 @@ class SingleProject extends Component {
       // Otherwise go through the standard procedure
       const projectId = this.props.match.params.projectId;
       singleProject(projectId).then((data) => {
+        console.log(data);
         if (data.error) {
           console.log(data.error);
         } else {
@@ -43,54 +45,64 @@ class SingleProject extends Component {
     }
   };
 
-  renderProject = (project) => {
-    if (project.itemIsPublic) {
-      return (
-        <Container>
-          <Card bg="Light" border="secondary">
-            <Card.Header>{project.title}</Card.Header>
-            <ListGroup variant="flush">
-              <ListGroup.Item>{project.about}</ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col sm={20}>
-                    <Image
-                      src={`/api/project/img/${this.state.projectId}`}
-                      alt=""
-                      rounded
-                      thumbnail
-                    />
-                  </Col>
-                  <Col lg={true}>{project.body}</Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-          <Row className="justify-content-md-center">
-            <Image
-              src={`/api/project/img/${this.state.projectId}`}
-              alt=""
-              rounded
-            />
-          </Row>
-          <Row className="justify-content-md-center">
-            <Image
-              src={`/api/project/img/${this.state.projectId}`}
-              alt=""
-              rounded
-            />
-          </Row>
-        </Container>
-      );
-    } else {
-      return (
-        <Container>
-          <ListGroup.Item variant="warning">
-            Project Is Currently Private
-          </ListGroup.Item>
-        </Container>
-      );
-    }
+  // renderProject = (project) => {
+  //   if (project.itemIsPublic) {
+  //     return (
+  //       <Container>
+  //         <Card bg="Light" border="secondary">
+  //           <Card.Header>{project.title}</Card.Header>
+  //           <ListGroup variant="flush">
+  //             <ListGroup.Item>{project.about}</ListGroup.Item>
+  //             <ListGroup.Item>
+  //               <Row>
+  //                 <Col sm={20}>
+  //                   <Image
+  //                     src={`/api/project/${this.state.projectId}/img`}
+  //                     alt=""
+  //                     rounded
+  //                     thumbnail
+  //                   />
+  //                 </Col>
+  //                 <Col lg={true}>{project.body}</Col>
+  //               </Row>
+  //             </ListGroup.Item>
+  //           </ListGroup>
+  //         </Card>
+  //         <Row className="justify-content-md-center">
+  //           <Image
+  //             src={`/api/project/img/${this.state.projectId}`}
+  //             alt=""
+  //             rounded
+  //           />
+  //         </Row>
+  //         <Row className="justify-content-md-center">
+  //           <Image
+  //             src={`/api/project/img/${this.state.projectId}`}
+  //             alt=""
+  //             rounded
+  //           />
+  //         </Row>
+  //       </Container>
+  //     );
+  //   } else {
+  //     return (
+  //       <Container>
+  //         <ListGroup.Item variant="warning">
+  //           Project Is Currently Private
+  //         </ListGroup.Item>
+  //       </Container>
+  //     );
+  //   }
+  // };
+
+  convertRTFtoHTML = (txt) => {
+    // replace **text** with <strong>text</strong>
+    txt = txt.replace(/\*\*([^*]+)\*\*/g, "<strong>$1</strong>");
+    // replace *text* with <i>text</i>
+    txt = txt.replace(/\*([^*]+)\*/g, "<i>$1</i>");
+    // replace [site](url) with <a href="url">site</a>
+    txt = txt.replace(/\[([^[]+)\]\(([^)]+)\)/g, "<a href='$2'>$1</a>");
+    return txt;
   };
 
   render() {
@@ -102,24 +114,49 @@ class SingleProject extends Component {
     if (!project) {
       return <Container>Loading...</Container>;
     }
-    const { title, about, body } = project;
+    const { title, about, body, additionalFiles } = project;
+    const formattedBody = this.convertRTFtoHTML(body);
     // const posterId = project.postedBy ? `/user/${project.postedBy._id}` : "";
     // const posterName = project.postedBy ? project.postedBy.name : "Unknown";
+
+    const files = additionalFiles.map((file, i) => {
+      const url = `http://localhost:5000/api/project/${this.state.projectId}/file/${i}`;
+      const displayName =
+        file.length > 25 ? file.substring(0, 22) + "..." : file;
+      return (
+        <div key={i}>
+          <a href={url}>{displayName}</a>
+        </div>
+      );
+    });
+    let filesDisplay;
+    if (files.length > 0) {
+      filesDisplay = (
+        <div className="pt-3">
+          <h5>Additional files</h5>
+          {files}
+        </div>
+      );
+    } else {
+      filesDisplay = null;
+    }
+
     return (
       <Container>
         <Card>
           <Card.Header>
-            <div className="text-center">
+            <HCenter>
               <h1>{title}</h1>
-            </div>
-            <hr />
-            <div className="text-center">{about}</div>
+            </HCenter>
+            <HCenter>
+              <i>{about}</i>
+            </HCenter>
           </Card.Header>
           <Row noGutters>
             <Col lg={6}>
               <Card.Body>
                 <Image
-                  src={`/api/project/img/${this.state.projectId}`}
+                  src={`/api/project/${this.state.projectId}/img`}
                   alt=""
                   style={{ width: "100%" }}
                   thumbnail
@@ -127,17 +164,12 @@ class SingleProject extends Component {
               </Card.Body>
             </Col>
             <Col lg={6}>
-              <Card.Body style={{ whiteSpace: "pre-line" }}>{body}</Card.Body>
+              <Card.Body style={{ whiteSpace: "pre-line" }}>
+                <div dangerouslySetInnerHTML={{ __html: formattedBody }}></div>
+                {filesDisplay}
+              </Card.Body>
             </Col>
           </Row>
-          <hr />
-          <Card.Body>
-            <h2>Gallery</h2>
-          </Card.Body>
-          <hr />
-          <Card.Body>
-            <h2>Additional files</h2>
-          </Card.Body>
           <Card.Footer className="text-muted">
             Last updated at some point who knows really
           </Card.Footer>
