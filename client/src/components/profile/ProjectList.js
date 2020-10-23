a596c6209ebd3e100bc20eec16fb127f284d2d07
hoangainhipham14 <hoangainhip@student.unimelb.edu.au>
2020-10-23 19:31:10 +1100

    Fixed path for createProject

--- client/src/components/profile/ProjectList.js
+++ client/src/components/profile/ProjectList.js
@@ -88,9 +88,7 @@ class ProjectList extends Component {
             )}
 
             {this.props.isAuth && (
-              <Button href={`/createProject/${this.props.username}`}>
-                Create Project
-              </Button>
+              <Button href="/createProject">Create Project</Button>
             )}
           </Container>
         </Col>
