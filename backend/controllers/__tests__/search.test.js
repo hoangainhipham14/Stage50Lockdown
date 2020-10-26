6e06bdf35541fdb4b68e8920dfd7e9bd9d0c51dd
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2020-10-26 21:09:11 +1100

    Small changes to fb login

---
+++ backend/controllers/__tests__/search.test.js
@@ -0,0 +1,11 @@
+// Project functions
+const {
+    userSearch,
+  } = require("../search");
+  
+  // Check they are all defined
+  describe("Project Validation Tests -> Check the files exist", function () {
+        it("should recognise all the functions exist", function () {
+         expect(userSearch);
+        })
+    });
