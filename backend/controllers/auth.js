64f486f57950bbb88c847b14eb9221f4bc84a015
Rowan Warneke <rowanwarneke@gmail.com>
2020-10-27 11:43:49 +1100

    fixed facebook

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -212,7 +212,10 @@ exports.fbSignin = (req, res) => {
       .catch((err) => console.log("Error saving user session:", err));
 
     // generate token
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { _id: user._id, username: user.username },
+      process.env.JWT_SECRET
+    );
 
     // use cookie to store token
     // expiry = current epoch time (seconds) + length of year
